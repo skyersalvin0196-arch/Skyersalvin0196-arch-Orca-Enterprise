1e0d7c912f5096a23dd7c085904b50cc536812a3
skyersalvin0196-arch <skyersalvin0196@gmail.com>
2026-09-24 12:29:54 -0500

    Add service worker for installable app

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>self.clients.claim());self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
